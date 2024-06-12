10acbb0891a63d8f523957a5fc90a479cf28d22e
cpetras01 <coleman.petras7@gmail.com>
2024-06-12 07:54:11 -0400

    Create M02-GPA Check.py

---
+++ M02-GPA Check.py
@@ -0,0 +1,45 @@
+# M02 Lab - Case Study: if... else and while
+# M02-GPA Check
+# Coleman Petas
+# SDEV 220
+# 12 June 2024
+
+# Write an app that accepts input of a student's last name and GPA
+# Take that information and test whether the student made the 
+# Dean's List or the Honor Roll
+
+# Allow input until 'ZZZ' is entered for student last name
+honorRollFirst = []
+honorRollLast = []
+deansListFirst = []
+deansListLast = []
+
+while True:
+    studentLast = input('Please enter student last name: ')
+    if studentLast == 'ZZZ':
+        break
+    else:
+        studentFirst = input('Please enter student First name: ')
+        studentGPA = float(input('Please enter student GPA: '))
+        if studentGPA >= 3.5:
+            deansListFirst.append(studentFirst)
+            deansListLast.append(studentLast)
+        elif studentGPA >= 3.25:
+            honorRollFirst.append(studentFirst)
+            honorRollLast.append(studentLast)
+        else:
+            pass
+
+def deans_list():
+    print('Deans List:')
+    deansList = zip(deansListFirst, deansListLast)
+    [print(i) for i in deansList]
+
+def honor_roll():
+    print('Honor Roll:')
+    honor_Roll = zip(honorRollFirst, honorRollLast)
+    [print(i) for i in honor_Roll]
+
+deans_list()
+print()
+honor_roll()
