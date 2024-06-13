0297589181e531f25e819db473e1bf96f767b50b
cpetras01 <coleman.petras7@gmail.com>
2024-06-13 14:42:35 -0400

    Update M02-GPA Check.py

--- M02-GPA Check.py
+++ M02-GPA Check.py
@@ -9,37 +9,40 @@
 # Dean's List or the Honor Roll
 
 # Allow input until 'ZZZ' is entered for student last name
+
 honorRollFirst = []
 honorRollLast = []
 deansListFirst = []
 deansListLast = []
 
-while True:
-    studentLast = input('Please enter student last name: ')
-    if studentLast == 'ZZZ':
-        break
-    else:
-        studentFirst = input('Please enter student First name: ')
-        studentGPA = float(input('Please enter student GPA: '))
-        if studentGPA >= 3.5:
-            deansListFirst.append(studentFirst)
-            deansListLast.append(studentLast)
-        elif studentGPA >= 3.25:
-            honorRollFirst.append(studentFirst)
-            honorRollLast.append(studentLast)
+def main():
+    while True:
+        studentLast = input('Please enter student last name: ')
+        if studentLast == 'ZZZ':
+            break
         else:
-            pass
+            studentFirst = input('Please enter student First name: ')
+            studentGPA = float(input('Please enter student GPA: '))
+            if studentGPA >= 3.5:
+                deansListFirst.append(studentFirst)
+                deansListLast.append(studentLast)
+            elif studentGPA >= 3.25:
+                honorRollFirst.append(studentFirst)
+                honorRollLast.append(studentLast)
+            else:
+                pass
 
 def deans_list():
     print('Deans List:')
     deansList = zip(deansListFirst, deansListLast)
     [print(i) for i in deansList]
+    print()
 
 def honor_roll():
     print('Honor Roll:')
     honor_Roll = zip(honorRollFirst, honorRollLast)
     [print(i) for i in honor_Roll]
 
+main()
 deans_list()
-print()
 honor_roll()
